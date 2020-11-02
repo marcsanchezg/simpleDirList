ed19feb6c0c43509190915ec75534c9230bddb50
root <root@marc.casa.local>
2020-11-02 14:25:52 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,19 @@
+/**
+ * Plugin Name:       Simple Directory List
+ * Plugin URI:        https://example.com/
+ * Description:       List a basic ftp directory to share files globally without having to upload them one by one via WordPress tools.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Marc Sanchez Garcia
+ * Author URI:        https://www.marcsanchezg.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       simpleDirList
+ * Domain Path:       /languages
+
+ <?php
+ 
+ 
+ 
+ ?>
